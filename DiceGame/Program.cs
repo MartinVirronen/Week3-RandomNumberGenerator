2772951961a2c6cb0830c9e9df18ee652efa6f70
Opilane TTHK <opilane@tthk.ee>
2021-09-15 09:38:27 +0300

    RandomNumberGenerator

---
+++ DiceGame/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus on kaks mängijat
+            //mõlemad mängijad viskavad täringut
+            //programm kontrollib, kumb mängija viskas rohkem
+            //mängija, kes viskas rohkem ongi võitja
+            //programm kuulutab võitjat
+
+            Random rndnumber = new Random();
+
+            string Playerone = ("Sebastian");
+            string Playertwo = ("Martin");
+
+            int Sebastianthrow = rndnumber.Next(1, 7);
+            int Martinthrow = rndnumber.Next(1, 7);
+
+            Console.WriteLine($"{Playerone} threw {Sebastianthrow};");
+            Console.WriteLine($"{Playertwo} threw {Martinthrow};");
+            if(Sebastianthrow > Martinthrow)
+            {
+                Console.WriteLine($"{Playerone} Wins!");
+
+            }
+           
+               else if (Sebastianthrow < Martinthrow)
+            {
+                Console.WriteLine($"{Playertwo} Wins!");
+                
+            }
+            else
+            {
+                Console.WriteLine("draw! let them try again!");
+
+            }
+
+        }
+
+    }
+}
+
+
---
+++ RandomNumberGenerator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndnumber = new Random();
+            int RandomNumber = rndnumber.Next(1, 11);//number 1 kuni 10
+            Console.WriteLine(RandomNumber);
+            if (RandomNumber > 5)
+            {
+                Console.WriteLine($"Juhuslik number on {RandomNumber}, see on suurem, kui 5.");
+            }
+            else if (RandomNumber < 5)
+            {
+                Console.WriteLine($"Juhuslik number on {RandomNumber}, see on väiksem kui 5.");
+            }
+            else
+            {
+                Console.WriteLine("Juhuslik number ongi 5.");
+            }
+        }  
+    }
+
+
+}
